a355ffe35e15380786329a76cc3acde0ac5c5118
Aidan Muller-Cohn <aidanmc95@gmail.com>
2020-04-08 07:51:57 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -30,7 +30,7 @@ def monopoly_with_fourth_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
-  current_hash = monopoly_with_second_tier
+  current_hash = monopoly_with_third_tier
   current_hash[:railroads][:names][:reading_railroad] = {:morgatge_value => 100}
   current_hash[:railroads][:names][:pennsylvania_railroad] = {:mortgage_value => 200}
   current_hash[:railroads][:names][:b_and_o_railroad] = {:mortgage_value => 400}
